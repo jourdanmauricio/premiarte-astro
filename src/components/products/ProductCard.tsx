c457159091bd6e900cc0dc9373203a534c10d964
mauricio <jourdanmauricio@gmail.com>
2025-09-23 16:37:47 -0300

    Add Styles

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -7,15 +7,17 @@ interface ProductCardProps {
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div className='group relative w-64'>
-      <a href={`/productos/${product.slug}`}>
+      <a href={`/productos/${product.slug}`} className='h-64 w-64'>
         <img
           src={product.images[0].url}
           alt={product.images[0].alt}
-          className='w-full h-full object-contain'
+          className='object-contain'
+          width={256}
+          height={256}
         />
-        <h4>{product.name}</h4>
-        <p>{product.categories[0].name}</p>
       </a>
+      <p className='pill my-2'>{product.categories[0].name}</p>
+      <h4 className='text-sm font-medium'>{product.name}</h4>
     </div>
   );
 };
